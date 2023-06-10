2ce35cc208f0d2d5da01cee0ac0cf3f7be20a649
Romain <romainyvernes@gmail.com>
2023-06-09 20:34:26 -0700

    Add initial controllers

---
+++ server/controllers/ProjectController.js
@@ -0,0 +1,21 @@
+export default {
+  getProjects: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  getProject: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  createProject: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  updateProject: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  deleteProject: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+};
---
+++ server/controllers/UserController.js
@@ -0,0 +1,17 @@
+export default {
+  getUsers: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  getUser: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  updateUser: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+
+  deleteUser: (req, res, next) => {
+    res.send("respond with a resource");
+  },
+};
