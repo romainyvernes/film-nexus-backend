cbde5a499db84db54687ef6590ac5033def0ec6a
Romain <romainyvernes@gmail.com>
2023-06-16 18:31:14 -0700

    Update project name

--- server/bin/www.js
+++ server/bin/www.js
@@ -7,7 +7,7 @@
 import app from '../app';
 import debugLib from 'debug';
 import http from 'http';
-const debug = debugLib('es6-express-template:server');
+const debug = debugLib('film-nexus-backend:server');
 
 /**
  * Get port from environment and store in Express.
