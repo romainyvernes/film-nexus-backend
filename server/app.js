ce2a31b2f359e991cb1a0ce35569d558145a652d
Romain <romainyvernes@gmail.com>
2023-06-16 21:15:12 -0700

    Remove unnecessary modules

--- server/app.js
+++ server/app.js
@@ -1,19 +1,15 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
-import logger from "morgan";
 import cors from "cors";
 import pool from "./db";
 import apiRouter from "./routes";
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public"), { index: false }));
 
 app.use("/api", apiRouter);
 
