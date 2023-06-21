eff93b1da0acfb8d48098d174b6b9e169ef3a626
Romain <romainyvernes@gmail.com>
2023-06-21 12:07:30 -0700

    Add project and project_members tables

--- server/database.sql
+++ server/database.sql
@@ -10,3 +10,17 @@ CREATE TABLE users (
   password TEXT NOT NULL,
   created_on TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+CREATE TABLE projects (
+  id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name TEXT NOT NULL,
+  created_on TIMESTAMPTZ NOT NULL DEFAULT now(),
+  creator_id UUID NOT NULL
+);
+
+CREATE TABLE project_members (
+  project_id UUID NOT NULL,
+  user_id UUID NOT NULL,
+  position TEXT NOT NULL,
+  is_admin BOOLEAN NOT NULL
+);
