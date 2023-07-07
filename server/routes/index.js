9f26e0969ec975cadeef927f768868a45477ae0e
Romain <romainyvernes@gmail.com>
2023-07-06 20:06:22 -0700

    Add members routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import usersRouter from "./users";
 import projectsRouter from "./projects";
 import { verifyToken } from "../middleware/jwt";
+import membersRouter from "./members";
 
 const router = Router();
 
 // prevent non-authenticated users from accessing below routes
 router.use(verifyToken);
 router.use("/users", usersRouter);
+router.use("/projects/:id/members", membersRouter);
 router.use("/projects", projectsRouter);
 
 export default router;
