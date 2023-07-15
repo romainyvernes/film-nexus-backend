a3c91ae5360464e1adf49a568d49e104d5a1d5ed
Romain <romainyvernes@gmail.com>
2023-07-14 20:58:33 -0700

    Add helmet middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import 'dotenv/config'
+import 'dotenv/config';
+import helmet from "helmet";
 import apiRouter from "./routes";
 import { sanitizeDataInput } from "./middleware/sanitization";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
