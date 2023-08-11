39ed08cfbeec4319c9afbd9a4e3058f086e60049
Romain <romainyvernes@gmail.com>
2023-08-10 23:25:20 -0700

    Add .env file for each environment type

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import 'dotenv/config';
+import dotenv from "dotenv";
 import helmet from "helmet";
 import passport from "passport";
 import apiRouter from "./routes";
 import { sanitizeDataInput } from "./middleware/sanitization";
 import "./middleware/passport";
+
+// Load environment variables based on the current environment
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.prod" });
+} else {
+  dotenv.config({ path: ".env.local" });
+}
 
 const app = express();
 
